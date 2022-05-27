a2dba2c02551b7c96fe12f0b9c14176cb8a5cfc0
hossain <hossainmd07054@gmail.com>
2022-05-27 00:34:30 -0400

    hi

--- src/main/java/Java/array.java
+++ src/main/java/Java/array.java
@@ -1,24 +1,24 @@
 package Java;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class array {
 
 	public static void main(String[] args) {
 		 String[] fruts = {"Apple","Orange","Bannana","Bannana"};
 		  System.out.println (fruts.length); 
-		  System.out.println (fruts);
+		  System.out.println (fruts[0]);
 		  
 		  
 		  
-		  ArrayList obj = new ArrayList(); 
+		/*  ArrayList obj = new ArrayList(); 
 	 	 	 obj.add("sugar");
 	 	 	 obj.add("salt");  	 	 
 	 	 	 obj.add(123);  	 	 
 	 	 	 obj.add(5.5);  	 	 
 	 	 	 obj.add("sugar"); 
 	System.out.println(obj.size()); 
-	System.out.println(obj);
+	System.out.println(obj);*/
  
 		
 	
